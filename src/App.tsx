42a132c7cbc766e3884284cf634a976cd883744a
sarathrajan922 <sarathrajan922@gmail.com>
2023-10-09 23:03:29 +0530

    tailwind config done

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import socketIO from 'socket.io-client';
-
- 
 const WS = 'http://localhost:8080';
-
 function App() {
 
   useEffect(()=>{
     socketIO(WS);
   },[]);
   return (
-    <div className="App">
-      <button>start new meeting</button>
+    <div className="App  flex items-center justify-center w-screen h-screen">
+      <button className='bg-slate-500 text-white py-2 px-8 rounded-lg'>start new meeting</button>
     </div>
   );
 }
