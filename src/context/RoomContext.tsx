021fa72e68836f9d7b825fd4630eff52c81051d2
sarathrajan922 <sarathrajan922@gmail.com>
2023-10-09 23:38:59 +0530

    joinRoom fn created

--- src/context/RoomContext.tsx
+++ src/context/RoomContext.tsx
@@ -1,10 +1,12 @@
 import socketIOClient from 'socket.io-client';
-import { createContext } from 'react';
-
-
-const roomContext= createContext<null | any>(null);
-
- 
+import React, { createContext ,ReactNode } from 'react';
 const WS = 'http://localhost:8080';
 
+
+export const RoomContext= createContext<null | any>(null);
+
 const ws = socketIOClient(WS)
+interface RoomProviderProps {
+    children: ReactNode;
+  }
+export const RoomProvider: React.FunctionComponent<RoomProviderProps> =({ children })=><RoomContext.Provider value={{ ws }}>{children}</RoomContext.Provider>
