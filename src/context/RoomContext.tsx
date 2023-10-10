b749b1e05256313fd7166fc6448783ece2d10080
sarathrajan922 <sarathrajan922@gmail.com>
2023-10-10 21:31:14 +0530

    peerjs and uuid installed in client side

--- src/context/RoomContext.tsx
+++ src/context/RoomContext.tsx
@@ -1,6 +1,8 @@
 import socketIOClient from 'socket.io-client';
-import React, { createContext ,ReactNode, useEffect } from 'react';
+import React, { createContext ,ReactNode, useEffect, useState } from 'react';
 import {useNavigate} from 'react-router-dom'
+import Peer from 'peerjs';
+import {v4 as uuidV4 } from 'uuid';
 const WS = 'http://localhost:8080';
 
 
@@ -12,13 +14,18 @@ interface RoomProviderProps {
   }
 export const RoomProvider: React.FunctionComponent<RoomProviderProps> =({ children })=>{
     const navigate = useNavigate();
+    const [me, setMe] = useState<Peer>()
  const enterRoom = ({roomId}:{roomId: 'string'})=>{
     console.log(roomId)
     navigate(`/room/${roomId}`)
  }
 useEffect(()=>{
+  const meId = uuidV4();
+
+  const peer = new Peer(meId);
+  setMe(peer)
     ws.on('room-created', enterRoom)
 },[])
 return(
-<RoomContext.Provider value={{ ws }}>{children}</RoomContext.Provider>
+<RoomContext.Provider value={{ ws, me }}>{children}</RoomContext.Provider>
 )}
